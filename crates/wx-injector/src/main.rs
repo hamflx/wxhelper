d8d29d9b3032bdfdf5d8ccdaf1481d083841eee1
hamflx <a@hamflx.cn>
2024-01-30 23:47:04 +0800

    feat: 注入到 WeChat 进程。

--- crates/wx-injector/src/main.rs
+++ crates/wx-injector/src/main.rs
@@ -1,3 +1,33 @@
+use std::path::Path;
+
+use sysinfo::System;
+use windows::Win32::{
+    Foundation::BOOL,
+    System::Threading::{OpenProcess, PROCESS_ALL_ACCESS},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let mut sys = System::new_all();
+
+    sys.refresh_processes();
+
+    let process = sys.processes_by_name("WeChat.exe").next().unwrap();
+    let pid = process.pid().as_u32();
+
+    let handle = unsafe { OpenProcess(PROCESS_ALL_ACCESS, BOOL(0), pid) }.unwrap();
+    let handle = injectors::process::ProcessHandle::from_handle(handle.0);
+    handle
+        .inject_to_process(
+            &None,
+            Path::new(env!("CARGO_MANIFEST_DIR"))
+                .parent()
+                .unwrap()
+                .parent()
+                .unwrap()
+                .join("target\\debug\\wx_payload.dll")
+                .to_str()
+                .unwrap(),
+        )
+        .map_err(|_| std::io::Error::last_os_error())
+        .unwrap();
 }
--- crates/wx-payload/src/lib.rs
+++ crates/wx-payload/src/lib.rs
@@ -2,6 +2,11 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+#[no_mangle]
+pub extern "system" fn enable_hook(ptr: usize) -> usize {
+    0
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
