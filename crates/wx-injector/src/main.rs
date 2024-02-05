851ef6d16a8639dd1bbf0e08777b0cdfe5a43b3b
hamflx <a@hamflx.cn>
2024-02-05 22:16:22 +0800

    feat: 打包 dll。

--- crates/wx-injector/src/main.rs
+++ crates/wx-injector/src/main.rs
@@ -1,12 +1,16 @@
 use std::path::Path;
 
-use log::info;
+use anyhow::{anyhow, Result};
+use log::{error, info};
 use simplelog::{ColorChoice, Config, LevelFilter, TermLogger, TerminalMode};
 use sysinfo::System;
+use tempfile::tempdir;
 use windows::Win32::{
     Foundation::BOOL,
     System::Threading::{OpenProcess, PROCESS_ALL_ACCESS},
 };
+
+const PAYLOAD: &[u8] = include_bytes!(env!("CARGO_CDYLIB_FILE_WX_PAYLOAD_wx-payload"));
 
 fn main() {
     TermLogger::init(
@@ -19,17 +23,32 @@ fn main() {
 
     info!("Search WeChat.exe ...");
 
+    match start() {
+        Ok(_) => info!("Exit"),
+        Err(err) => error!("Error: {err}"),
+    }
+}
+
+fn start() -> Result<()> {
+    let dir = tempdir()?;
+    let dll_path = dir.path().join("wx-extension.dll");
+    std::fs::write(&dll_path, PAYLOAD)?;
+
     let mut sys = System::new_all();
-
     sys.refresh_processes();
-
-    let process = sys.processes_by_name("WeChat.exe").next().unwrap();
+    let process = sys
+        .processes_by_name("WeChat.exe")
+        .next()
+        .ok_or_else(|| anyhow!("No WeChat process found"))?;
     let pid = process.pid().as_u32();
-
     let handle = unsafe { OpenProcess(PROCESS_ALL_ACCESS, BOOL(0), pid) }.unwrap();
     let handle = injectors::process::ProcessHandle::from_handle(handle.0);
     handle
-        .inject_to_process(&None, env!("CARGO_CDYLIB_FILE_WX_PAYLOAD_wx-payload"))
-        .map_err(|_| std::io::Error::last_os_error())
-        .unwrap();
+        .inject_to_process(
+            &None,
+            std::str::from_utf8(dll_path.as_os_str().as_encoded_bytes())?,
+        )
+        .map_err(|_| std::io::Error::last_os_error())?;
+
+    Ok(())
 }
